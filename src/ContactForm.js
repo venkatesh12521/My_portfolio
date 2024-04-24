bcf873b0de09dde049a995e0e3c0ca92762fb7da
venkatesh <141809553+venkatesh12521@users.noreply.github.com>
2024-04-24 13:07:47 +0530

    Update ContactForm.js

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -42,19 +42,19 @@ const Contact = () => {
       </div>
 
       {/* card*/}
-      <div className="card-wrapper w-[90%] sm:w-[100%] mx-auto mt-5 flex items-center justify-center sm:flex-col">
-        <div className="left w-[70%] flex-1 flex items-center justify-center sm:flex-col sm:w-full">
-          <div className="flex-3 w-1/2 gap-3 flex items-end justify-end  flex-col sm:w-3/4">
+      <div className=" md:flex items-center justify-center ">
+        <div className=" md:flex-1 md:flex items-center justify-center ">
+          <div className=" ">
             <div data-aos="zoom-in">
-              <h1 className="text-5xl font-bold sm:text-3xl">You Need</h1>
-              <h3 className="text-xl sm:text-lg">
+              <h1 className="text-xl font-bold text-center sm:text-3xl">You Need</h1>
+              <h3 className="text-xl sm:text-lg  text-center">
                 Beautiful design for your website leave a request
               </h3>
             </div>
           </div>
-          <div className="flex p-5 items-center justify-center ">
-            <button data-aos="zoom-in" className="text-yellow-500 font-extrabold text-3xl p-2 rounded-lg shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] ">
-              <BsArrowRight className=" md:rotate-90" />
+          <div className="md:flex p-5 flex items-center justify-center ">
+            <button data-aos="zoom-in" className="text-yellow-500 font-extrabold  text-3xl p-2 rounded-lg shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] ">
+              <BsArrowRight className=" md:rotate-30 rotate-90 h-6" />
             </button>
           </div>
         </div>
